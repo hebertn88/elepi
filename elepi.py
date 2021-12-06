1204ad50005d15af4c28736ff279c9e269240938
Hebert do Nascimento <hebertn88@gmail.com>
2021-12-06 15:48:31 -0300

    alteracoes 06122021

--- elepi.py
+++ elepi.py
@@ -1,81 +1,27 @@
-#banco de dados
-#import sqlite3
-#from sqlite3 import Error
-
+#import para verificacao do Arquivo do Banco de Dados
 import os
 
 #criptografia da senha do usuario
 from passlib.hash import pbkdf2_sha256
+from sqlalchemy.orm import session
 
+#import do Arquivo models.py
 import models as db
 
-if not os.path.isfile('data.db'):
-    print('criar banco de dados')
-    db.base.metadata.create_all(db.engine)
-    
-db.usuario_inserir('hebert3', 'senha')
-print(db.usuario_valida('hebert', '123456'))
+from sqlite3 import Error
 
 
-"""
-class Conexao(object):
+if not os.path.isfile('./data/data.db'):
+    print('Banco de dados não encontrado.')
+    print('Criando novo Banco de dados...')
 
-    def __init__(self, database):
-        self.database = database
-
-
-    def conecta(self):
-       self.connection = sqlite3.connect(self.database)
-       self.cursor = self.connection.cursor()
-
-
-    def commit(self):
-        self.connection.commit()
+    try:
+        db.Base.metadata.create_all(db.engine)
+        print('Banco de Dados criado com sucesso!')
+    except Error as erro:
+        print('inicio erro banco')
+        print(erro)
+        print('fim erro banco')
 
 
-    def encerra(self):
-        self.connection.close()
-
-
-    def consultar(self, sql):
-        try:
-            self.conecta()
-            self.cursor.execute(sql)
-            dados = self.cursor.fetchall()
-            self.encerra()
-            return dados
-        except Error as erro:
-            print(erro)
-  
-
-    def inserir(self,sql):
-        try:
-            self.conecta()
-            self.cursor.execute(sql)
-            self.commit()
-            self.encerra()
-        except Error as erro:
-            print(erro)
-
-
-def senha_criptografa(senha):
-    senha = pbkdf2_sha256.hash(senha)
-    return senha
-
-
-def senha_verifica(senha1, cript1):
-    return pbkdf2_sha256.verify(senha1, cript1)
-
-
-db = Conexao('data.db') #instancia classe conexao para manuseio do banco de dados
-
-senha = senha_criptografa('123456') #criptografa senha
-db.inserir(f"INSERT INTO usuarios (usuario, senha) VALUES ('Maria', '{senha}')") #insere usuario
-
-print(db.consultar('select * from usuarios')) #mostra todos usuarios
-
-dado = db.consultar("select senha from usuarios where usuario like 'hebert'") #captura senha do usuario pesquisado
-
-print(senha_verifica('123456', dado[0][0])) #verifica senha do usuario
-
-"""
+db.usuario_inserir('hebert', '123456')
--- models.py
+++ models.py
@@ -3,21 +3,21 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+#import para exibição de Erros SQLite
+from sqlite3 import Error
+
+#import modulo de criptografia
 from passlib.hash import pbkdf2_sha256
 
-#class Conexao(object):
-
-#def __init__(self):
 
 #Cria conexão e sessão com banco de dados
-engine = create_engine("sqlite:///data.db")
+engine = create_engine("sqlite:///data/data.db", echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
-base = declarative_base()
+Base = declarative_base()
 
 
-class Usuario(base):
-
+class Usuarios(Base):
     __tablename__ = 'usuarios'
     
     id_usuario = Column(Integer, primary_key=True)
@@ -27,45 +27,21 @@ class Usuario(base):
 
 def usuario_inserir(pusuario, psenha):
     psenha = pbkdf2_sha256.hash(psenha)
-    usuario = Usuario(usuario = psenha, senha = psenha)
-    session.add(usuario)
-    session.commit()
+    usuario = Usuarios(usuario = pusuario, senha = psenha)
+    
+    if session.query(Usuarios).filter_by(usuario=pusuario).one_or_none():
+        return False
+
+    try:
+        session.add(usuario)
+        session.commit()
+        return True
+    except Error as erro:
+        return erro
 
 
 def usuario_valida(pusuario, psenha):
-    usuario = session.query(Usuario).filter_by(usuario=pusuario).one_or_none()
-
-    #print(query.usuario)
-
-    #usuario = {}
-
-    #for u in query:
-    #    usuario['usuario'] = u.usuario
-    #    usuario['senha'] = u.senha
+    usuario = session.query(Usuarios).filter_by(usuario=pusuario).one_or_none()
     if usuario:
         return pbkdf2_sha256.verify(psenha, usuario.senha)
 
-
-#result = con.session.execute("select * from usuarios")
-#for r in result:
-#    print(r)
-"""
-metadata_obj = MetaData()
-
-user = Table('user', metadata_obj,
-    Column('user_id', Integer, primary_key=True),
-    Column('user_name', String(16), nullable=False),
-    Column('email_address', String(60), key='email'),
-    Column('nickname', String(50), nullable=False)
-)
-
-user_prefs = Table('user_prefs', metadata_obj,
-    Column('pref_id', Integer, primary_key=True),
-    Column('user_id', Integer, ForeignKey("user.user_id"), nullable=False),
-    Column('pref_name', String(40), nullable=False),
-    Column('pref_value', String(100))
-)
-
-SQLmetadata_obj.create_all(engine)
-
-"""
